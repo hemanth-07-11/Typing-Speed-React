3ce9f649e10c7c20ba3e4bafed9ec97ec04793be
Hemanth N <hemanthnov2001@gmail.com>
2021-06-18 18:57:31 +0530

    Updates

---
+++ precache-manifest.55dea20c775775254e21617c3bfa80cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b08b3bca352763004c63f551dce89db",
+    "url": "/Typing-Speed-React/index.html"
+  },
+  {
+    "revision": "32cb88d8361d8199982c",
+    "url": "/Typing-Speed-React/static/css/main.93771a0a.chunk.css"
+  },
+  {
+    "revision": "26978a316f29280b9576",
+    "url": "/Typing-Speed-React/static/js/2.5b3fd2cd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Typing-Speed-React/static/js/2.5b3fd2cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cb88d8361d8199982c",
+    "url": "/Typing-Speed-React/static/js/main.30feb6a6.chunk.js"
+  },
+  {
+    "revision": "dc9601c8ee81e6a08ba3",
+    "url": "/Typing-Speed-React/static/js/runtime-main.8ef21c32.js"
+  },
+  {
+    "revision": "1f766ecaae4d48e355a557b455ea5049",
+    "url": "/Typing-Speed-React/static/media/speed.1f766eca.svg"
+  },
+  {
+    "revision": "a17d45682a3848283a0750d2a05e8a35",
+    "url": "/Typing-Speed-React/static/media/target.a17d4568.svg"
+  }
+]);
